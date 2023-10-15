66fa01ebec2b0468522bd6064d71bdf49fa6149e
andreas-masaoutis <andreas.masaoutis@protonmail.ch>
2023-10-15 16:47:55 +0300

    feat: helpers for reading and writing CSV files
    Effectively a wrapper around csv reader, for tasks done often
    
    TASK_ID: formatting1, modularise
    STORY_ID: System_refactor

---
+++ main/sections/readers_n_writers/readers_n_writers.py
@@ -0,0 +1,67 @@
+"""The helper functions for reading and writing CSV files
+
+"""
+
+import csv
+
+
+def initial_reader(a_file_path):
+    """Read a CSV and return a list of lines
+    
+    Args:
+        Path to csv file - a string
+
+    Returns
+        A list of lists that in our case have the following format:
+        [
+            ['user_id','event_type','event_time'],
+            ['user_id','event_type','event_time'],
+            ...
+        ]
+    Raises:
+
+    """
+
+    line_collection = []
+
+    with open(a_file_path, "r") as the_input_file:
+        the_input_file_reader = csv.reader(the_input_file)
+        ## Get rid of the header
+        next(the_input_file_reader)
+
+        for line in the_input_file_reader:
+            user_id = line[0]
+            event_type = line[1]
+            event_time = line[2].strip("\n")
+
+            line_collection.append([user_id, event_type, event_time])
+
+    return line_collection
+
+
+def final_writer(a_file_path, an_iterator, a_header):
+    """Given a list of list, you'll get a CSV file
+    
+    Args:
+        Path to csv file - a string
+        A list of lists that in our case have the following format:
+        [
+            ['user_id','event_type','event_time'],
+            ['user_id','event_type','event_time'],
+            ...
+        ]
+        A list of values for the CSV header
+
+    Returns:
+        A CSV file at the filepath provides, with the header as first line and the list content as rows
+
+    Raises:
+        
+
+    """
+
+    with open(a_file_path, "w") as the_output_file:
+        the_output_file_writer = csv.writer(the_output_file)
+        the_output_file_writer.writerow(a_header)
+        for the_line in an_iterator:
+            the_output_file_writer.writerow(the_line)
